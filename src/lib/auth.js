8daffd70b17d41ffc219c2b86d1c6466dfd5e5ba
Maria Saavedra <cotesaavedra@github.com>
2022-03-15 18:14:17 -0300

    formularios de registro y login, migrar la función de logeo cn google a auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,4 +1,4 @@
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, signInWithRedirect } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
 //REGISTRO
 export const register = (email, password) =>{
     const auth = getAuth();
@@ -22,13 +22,33 @@ export const login = (email, password) => {
         .then((userCredential) => {
             console.log("login exitoso");
             const user = userCredential.user;
-            console.log(user);
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
-            console.log("login erroneo  ");
-            console.log(errorCode);
-            console.log(errorMessage);
+            console.log("login erroneo");
         })
+}
+//GOOGLE
+export const authGoogle = (provider) => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+    .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    console.log("usuario ingresa");
+    // ...
+    }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+    })
 }
--- src/main.js
+++ src/main.js
@@ -1,46 +1,37 @@
 // import { baseDeDatos } from './index.html';
 // import { obtenerRecetas } from './lib/firebase-base-de-datos.js';
-import { register, login} from "./lib/auth.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
-import {changeView} from './view-controler/router.js'
+import { register, login, authGoogle} from "./lib/auth.js";
+import { GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
+import { changeView } from './view-controler/router.js'
 // obtenerRecetas(baseDeDatos).then((recetas) => {
 //   console.log(recetas);
 // });
 
-const signupForm = document.querySelector("#signup-form");
-
-signupForm.addEventListener("submit", (e) => {
+const registerForm = document.querySelector("#register-form");
+registerForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const email = document.querySelector("#signup-email").value;
-    const password = document.querySelector("#signup-password").value;
+    const email = document.querySelector("#register-email").value;
+    const password = document.querySelector("#register-password").value;
+    console.log(email, password);
+    register(email,password);
+});
+
+const signUpForm = document.querySelector("#signUp-form");
+signUpForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const email = document.querySelector("#signUp-email").value;
+    const password = document.querySelector("#signUp-password").value;
     console.log(email, password);
     login(email, password);
-    register(email,password);
-})
+});
+
+
 const googleButton = document.querySelector("#google");
 const provider = new GoogleAuthProvider();  
 googleButton.addEventListener("click", e => {
-    const auth = getAuth();
-    signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    console.log("usuario ingresa");
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  })
+  authGoogle(provider);
 })
 
 const init = () => {
